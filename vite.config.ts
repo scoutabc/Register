10ede8562fef19ce04a03aad2e86059574ecf949
scoutabc <scout_169@163.com>
2025-08-22 10:30:24 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/register/',
+  base:'/Register/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
